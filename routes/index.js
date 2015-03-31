856d86e649b113bc40a00231f743b52d27f401b0
Jordan <skorlir@gmail.com>
2015-03-30 23:05:08 -0500

    block array types

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,7 @@ function paramifyQuery(q) {
 
   for (var k in q) {
     console.log(q);
-    if ( ! q.hasOwnProperty(k)) continue;
+    if ( typeof k !== typeof [] ) continue;
     if ( k == 'name' ) {
       ns = q[k].split(' '), p = p.concat([prm('name.first', ns[0]), prm('name.last', ns[1] || '')]);
       continue;
